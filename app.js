4dc79bb9434122706bea3d189f91d40c66752d12
RafaPatino01 <RafaPatino01@outlook.com>
2022-04-24 10:36:12 -0500

    Initial files

---
+++ app.js
@@ -0,0 +1,43 @@
+// Required packages
+const express = require('express')
+const cheerio = require('cheerio')
+const request = require('request-promise')
+
+const app = express()
+const port = 3000
+
+// Express Routing
+
+app.get('/', (req, res) => {
+    res.send($.html())
+})
+
+app.listen(port, () => {
+console.log(`Example app listening on port ${port}`)
+})
+
+// Scrapping Stuff
+let $ = cheerio.load("loading...")
+let token_names = []
+let crypto_names = []
+
+async function init() {
+    $ = await request({
+        uri: 'https://www.coingecko.com/',
+        transform: body => cheerio.load(body)
+    })
+
+    const tokens = $('.tw-hidden.d-lg-inline.font-normal.text-3xs.ml-2').each((i, element)=>{
+        token_names.push($(element).text().trim())
+    })
+    const names = $('.tw-hidden.font-bold.tw-items-center.tw-justify-between').each((i, element)=>{
+        crypto_names.push($(element).text().trim())
+    })
+    console.log(token_names)
+    console.log(crypto_names)
+
+}
+
+init()
+
+
